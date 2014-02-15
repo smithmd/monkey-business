21dd3d5dd4ff4f4f31e3381b887f91b7242900e6
Michael Smith <mike@noiseparticles.com>
2014-02-15 17:59:08 -0500

    starting to create some js prototype objects

--- index.php
+++ index.php
@@ -4,8 +4,12 @@
         
     </head>
     <body>
-      <a id="barrel"></a>
-      <div id="upgrades"></div>
+      <div id="bananas"><span id="bCounter">0</span> bananas</div>
+      <a id="barrel">This is the barrel</a>
+      <div id="upgrades">
+        <a>upgrade 1</a>
+        <a>upgrade 2</a>
+      </div>
       <?php include 'include/scripts.inc' ?>
     </body>
 </html>
---
+++ js/game.js
@@ -0,0 +1,13 @@
+/**
+ * Created by msmith on 2/15/14.
+ */
+
+var Barrel = Class.create();
+Barrel.prototype = {
+  initialize: function (name) {
+    this.monkeyType = 0;
+  }
+};
+
+var bananas = 0;
+
