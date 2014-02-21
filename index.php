7c59779c63c1504d6c113b0da2ee07d011b44062
Michael Smith <mike@noiseparticles.com>
2014-02-20 19:29:20 -0500

    fixed lines again

--- index.php
+++ index.php
@@ -1,30 +1,32 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        
-    </head>
-    <body>
-      <div id="scoreboard">
-        <span id="bCounter">0</span> bananas<br />
-        <span id="bStrength">0</span> beak strength<br />
-      </div>
-      <a href="javascript:void(0);" onclick="barrelClick()" id="barrel"><img src="img/barrel.jpg" /></a>
-      <div id="upgrades">
-        <a href="javascript:void(0);" onclick="addWoodpecker()">Buy a Basic Woodpecker (<span id="bCost">15</span> bananas)</a><br />
-        <a>upgrade 2</a>
-      </div>
-      <div id="monkeys">
-      </div>
-      <?php include 'include/scripts.inc' ?>
-      <script type="application/javascript">
-        document.observe("dom:loaded", function () {
-            var monkeyList = '<ul>';
-            monkeys.each( function (pair) {
-                monkeyList += '<li>' + pair.value.type + '</li>';
-            });
-            monkeyList += '</ul>';
-            $('monkeys').innerHTML = monkeyList;
+  <head>
+
+  </head>
+  <body>
+    <div id="scoreboard">
+      <span id="bCounter">0</span> bananas<br/>
+      <span id="bStrength">0</span> beak strength<br/>
+    </div>
+    <a href="javascript:void(0);" onclick="barrelClick()" id="barrel"><img src="img/barrel.jpg"/></a>
+
+    <div id="upgrades">
+      <a href="javascript:void(0);" onclick="addWoodpecker()">Buy a Basic Woodpecker (<span id="bCost">15</span>
+        bananas)</a><br/>
+      <a>upgrade 2</a>
+    </div>
+    <div id="monkeys">
+    </div>
+    <?php include 'include/scripts.inc' ?>
+    <script type="application/javascript">
+      document.observe("dom:loaded", function () {
+        var monkeyList = '<ul>';
+        monkeys.each(function (pair) {
+          monkeyList += '<li>' + pair.value.type + '</li>';
         });
-      </script>
-    </body>
+        monkeyList += '</ul>';
+        $('monkeys').innerHTML = monkeyList;
+      });
+    </script>
+  </body>
 </html>
